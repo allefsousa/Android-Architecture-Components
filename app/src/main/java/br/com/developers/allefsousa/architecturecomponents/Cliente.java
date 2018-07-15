b9e16a1869c2ab68fc3af2ab6d65f0cb9fc0bca1
Allef sousa <allefsousa_1@hotmail.com>
2018-07-15 20:54:34 -0300

    Entidade Table banco criada

---
+++ app/src/main/java/br/com/developers/allefsousa/architecturecomponents/Cliente.java
@@ -0,0 +1,51 @@
+package br.com.developers.allefsousa.architecturecomponents;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by allef on 15/07/2018.
+ */
+
+
+@Entity(tableName = "cliente_table")
+public class Cliente {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "name")
+    private String nome;
+    private String sobrenome;
+    @NonNull
+    private String cpf;
+
+
+    @NonNull
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(@NonNull String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    @NonNull
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(@NonNull String cpf) {
+        this.cpf = cpf;
+    }
+}
+
--- app/src/main/java/br/com/developers/allefsousa/architecturecomponents/MainActivity.java
+++ app/src/main/java/br/com/developers/allefsousa/architecturecomponents/MainActivity.java
@@ -8,8 +8,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
