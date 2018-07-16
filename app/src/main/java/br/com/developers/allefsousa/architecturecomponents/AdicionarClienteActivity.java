2c17a187ac3b723972e1d9cfff0fbae958333cd8
Allef sousa <allefsousa_1@hotmail.com>
2018-07-15 22:11:55 -0300

    Layout ok

---
+++ app/src/main/java/br/com/developers/allefsousa/architecturecomponents/AdicionarClienteActivity.java
@@ -0,0 +1,13 @@
+package br.com.developers.allefsousa.architecturecomponents;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdicionarClienteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_cliente);
+    }
+}
